0363e95f4e6b8d9e477cde13c3a9575602b9996d
Tom Houlé <tom@tomhoule.com>
2021-11-06 13:07:02 +0100

    Add microtype package back

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
       pkgs = import nixpkgs { inherit system; };
       lib = pkgs.lib;
       texlive = with pkgs.texlive; (combine {
-        inherit scheme-basic mathpazo babel babel-english amsmath palatino;
+        inherit scheme-basic microtype mathpazo babel babel-english amsmath palatino;
       });
     in
     {
